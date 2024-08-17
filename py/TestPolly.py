36c222d864e0035dc951c6abaa025b1ccd74beaf
Ryan Jeronimus <ryanjeronimus@gmail.com>
2024-08-17 17:41:21 -0400

    adding test

---
+++ py/TestPolly.py
@@ -0,0 +1,7 @@
+from Polly import Speaker
+
+speaker = Speaker()
+
+
+speaker.GenerateSpeech(""""The future of technology is incredibly exciting. With advancements in artificial intelligence, we're on the brink of a new era where machines can learn, adapt, and even create. The possibilities are endless, and the impact on society will be profound."
+""")
